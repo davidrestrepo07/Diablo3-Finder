03d3c3c51c95e866dc8fffbf333d8c0731f7d951
David Restrepo <davidrestrepo0107@gmail.com>
2020-08-17 10:55:02 -0500

    Agregamos las librerias de bootstrap,numeral y axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
+// Paquetes de npm
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+// Archivos locales de nuestra App
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// CSS global
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 new Vue({
   router,
