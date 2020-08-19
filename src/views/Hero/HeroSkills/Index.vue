ed6a3c61e24eb453c5fec5eef2d4698d8769d013
David Restrepo <davidrestrepo0107@gmail.com>
2020-08-19 15:08:35 -0500

    Se volvieron asincronos los componentes en skills

--- src/views/Hero/HeroSkills/Index.vue
+++ src/views/Hero/HeroSkills/Index.vue
@@ -1,29 +1,59 @@
 <template>
   <div class="skills-wrapper mt-5">
     <h2 class="font-diablo">Skills</h2>
-    <hr class="bg-white">
+    <hr class="bg-white" />
 
+    <b-nav pills small>
+      <b-nav-item :active="!isPassiveSkillsActive" @click="changeComponent('ActiveSkills')">Active</b-nav-item>
+      <b-nav-item :active="isPassiveSkillsActive" @click="changeComponent('PassiveSkills')">Passive</b-nav-item>
+    </b-nav>
+
+    <keep-alive>
+      <component :is="activeComponent" :skills="componentProps" />
+    </keep-alive>
+
+    <!--
     <ActiveSkills :skills="skills.active"/>
     <hr>
     <PassiveSkills :skills="skills.passive"/>
-
+    -->
   </div>
 </template>
 
 <script>
-import ActiveSkills from './ActiveSkills'
-import PassiveSkills from './PassiveSkills'
-
 export default {
   name: 'HeroSkills',
+  data () {
+    return {
+      activeComponent: 'ActiveSkills'
+    }
+  },
   components: {
-    ActiveSkills,
-    PassiveSkills
+    ActiveSkills: () =>
+      import(/* webpackChunkName: "ActiveSkills" */ './ActiveSkills'),
+    PassiveSkills: () =>
+      import(/* webpackChunkName: "PassiveSkills" */ './PassiveSkills')
   },
   props: {
     skills: {
       required: true,
       type: Object
+    }
+  },
+  computed: {
+    componentProps () {
+      return this.activeComponent === 'ActiveSkills'
+        ? this.skills.active
+        : this.skills.passive
+    },
+    // Nos dice si el componente "HabilidadesPasivas" está activo o no
+    isPassiveSkillsActive () {
+      return this.activeComponent === 'PassiveSkills'
+    }
+  },
+  methods: {
+    changeComponent (component) {
+      this.activeComponent = component
     }
   }
 }
