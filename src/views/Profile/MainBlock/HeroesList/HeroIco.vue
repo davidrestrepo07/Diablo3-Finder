611195032a86cf046595400369d3c38a99363fe7
David Restrepo <davidrestrepo0107@gmail.com>
2020-08-19 13:33:27 -0500

    Se agrega la vista del hero con los atributos y las habilidades, ademas de crear nuevos mixins

--- src/views/Profile/MainBlock/HeroesList/HeroIco.vue
+++ src/views/Profile/MainBlock/HeroesList/HeroIco.vue
@@ -1,8 +1,17 @@
 <template>
-<div class="hero-ico d-flex align-items-center hover-cursor-pointer" @click="goToHero(hero.id)">    <span class="hero-name ml-2 font-weight-bold" :class="{'text-danger': hero.hardcore}">
-      {{ hero.name }}
-    </span>
-    <img v-if="hero.seasonal" src="@/assets/img/leaf.png" width="12px" class="ml-2" alt="seasonal_leaf">
+  <div class="hero-portrait-wrapper mb-5 mb-sm-0 hover-cursor-pointer" @click="goToHero(hero.id)">
+    <span class="hero-image border" :class="heroClassImg" />
+    <span
+      class="hero-name ml-2 font-weight-bold"
+      :class="{'text-danger': hero.hardcore}"
+    >{{ hero.name }}</span>
+    <img
+      v-if="hero.seasonal"
+      src="@/assets/img/leaf.png"
+      width="12px"
+      class="ml-2"
+      alt="seasonal_leaf"
+    />
   </div>
 </template>
 
@@ -28,16 +37,19 @@ export default {
 </script>
 
 <style lang="stylus">
-  .hero-ico
-    vertical-align middle
+.hero-ico {
+  vertical-align: middle;
 
-    .hero-image
-      width 30px
-      height 26px
-      display inline-block
-      background-size 210%
+  .hero-image {
+    width: 30px;
+    height: 26px;
+    display: inline-block;
+    background-size: 210%;
+  }
 
-    .hero-name
-      height 24px
-      display inline-block
+  .hero-name {
+    height: 24px;
+    display: inline-block;
+  }
+}
 </style>
